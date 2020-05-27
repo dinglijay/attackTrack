546326a9319941db81754bc5e707e21b73cc5cc5
Dylan <dinglijay@gmail.com>
2020-05-27 06:46:32 +0000

    env upgrade, fucking take a whole day

---
+++ env/build_run.sh
@@ -0,0 +1,10 @@
+docker build .  --build-arg SSH_PUBKEY="$(cat ~/.ssh/id_rsa.pub)" -t li-attack
+docker run -ti \
+        -p 6024:22 \
+        -p 6025:6006 \
+        --hostname attackSer2 \
+        --name li-attack \
+        --gpus 'all' \
+        -v /home/li:/workspace \
+        -v /mnt/DataServer/li:/DataServer \
+        li-attack
