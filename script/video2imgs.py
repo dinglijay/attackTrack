1a267ae7f64d9d223c2239d7433653823d9754de
Dylan <dinglijay@gmail.com>
2020-06-04 02:40:49 -0700

    gen bbox gt and chessboard result file

--- script/video2imgs.py
+++ script/video2imgs.py
@@ -16,14 +16,12 @@ def main(video_file, rotate=False):
         rval, frame = vc.read()
         if not rval:
             break
-        # cv2.imshow('im', frame)
-
         if rotate:
             frame = frame.transpose(1,0,2)
         out = cv2.imwrite(dir_path+'_imgs/' + '{0:04d}'.format(c) + '.jpg', frame)
         print(out, c, dir_path+'_imgs/' + '{0:04d}'.format(c) + '.jpg')
         c = c + 1
-        cv2.waitKey(1)
+
     vc.release()
 
 if __name__ == '__main__':
