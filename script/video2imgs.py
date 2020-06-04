2d438ec7090e484644c5bbf13b70000cb8b1ff9a
Dylan <dinglijay@gmail.com>
2020-06-04 00:05:57 -0700

    padding imgs name to 04d

--- script/video2imgs.py
+++ script/video2imgs.py
@@ -20,8 +20,8 @@ def main(video_file, rotate=False):
 
         if rotate:
             frame = frame.transpose(1,0,2)
-        out = cv2.imwrite(dir_path+'_imgs/' + str(c) + '.jpg', frame)
-        print(out, c, Path(dir_path+'_imgs') / (str(c) + '.jpg'))
+        out = cv2.imwrite(dir_path+'_imgs/' + '{0:04d}'.format(c) + '.jpg', frame)
+        print(out, c, dir_path+'_imgs/' + '{0:04d}'.format(c) + '.jpg')
         c = c + 1
         cv2.waitKey(1)
     vc.release()
