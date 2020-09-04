08207a9090e87e606bd0afe3ef7a2b500e2480c6
Dylan <dinglijay@gmail.com>
2020-09-04 13:30:33 -0700

    sync

--- export.sh
+++ export.sh
@@ -7,5 +7,5 @@ export PYTHONPATH=$PWD:$PYTHONPATH
 cd $SiamMask/experiments/siammask_sharp
 export PYTHONPATH=$PWD:$PYTHONPATH
 cd ../../../attackTrack
-# export DISPLAY=saimServer2:10.0
+export DISPLAY=attackSer1:10.0
 # export CUDA_VISIBLE_DEVICES=1
